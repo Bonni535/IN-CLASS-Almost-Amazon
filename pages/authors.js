1a9d05d6276739d43dbba74d5e6db74d2d1a2cfb
Luca Bonini <luca14bonini@gmail.com>
2023-08-31 15:37:10 -0500

    added all the functionalities

--- pages/authors.js
+++ pages/authors.js
@@ -1,19 +1,20 @@
-import clearDom from '../utils/clearDom';
-import renderToDOM from '../utils/renderToDom';
+import clearDom from "../utils/clearDom";
+import renderToDOM from "../utils/renderToDom";
 
 const emptyAuthors = () => {
-  const domString = '<h1>No Authors</h1>';
-  renderToDOM('#store', domString);
+  const domString = "<h1>No Authors</h1>";
+  renderToDOM("#store", domString);
 };
 
 const showAuthors = (array) => {
   clearDom();
 
-  const btnString = '<button class="btn btn-success btn-lg mb-4" id="add-author-btn">Add An Author</button>';
+  const btnString =
+    '<button class="btn btn-success btn-lg mb-4" id="add-author-btn">Add An Author</button>';
 
-  renderToDOM('#add-button', btnString);
+  renderToDOM("#add-button", btnString);
 
-  let domString = '';
+  let domString = "";
   array.forEach((item) => {
     domString += `
     <div class="card" style="width: 18rem;">
@@ -22,13 +23,13 @@ const showAuthors = (array) => {
         <h6 class="card-subtitle mb-2 text-muted">${item.email}</h6>
         <hr>
         <i class="btn btn-success fas fa-eye" id="view-author-btn--${item.firebaseKey}"></i>
-        <i class="fas fa-edit btn btn-info" id="update-author--${item.firebaseKey}"></i>
+        <i class="fas fa-edit btn btn-info" id="edit-author-btn--${item.firebaseKey}"></i>
         <i class="btn btn-danger fas fa-trash-alt" id="delete-author-btn--${item.firebaseKey}"></i>
       </div>
     </div>
     `;
   });
-  renderToDOM('#store', domString);
+  renderToDOM("#store", domString);
 };
 
 export { showAuthors, emptyAuthors };
